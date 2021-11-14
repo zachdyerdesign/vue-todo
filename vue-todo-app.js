c66b3274c587a11f4a301cdc63e58d8b64455d97
Zach Dyer <zach.dyer@gmail.com>
2021-11-14 00:50:04 -0600

    Update vue-todo-app.js

--- vue-todo-app.js
+++ vue-todo-app.js
@@ -1,77 +1,78 @@
 var app4 = new Vue({
-    el: '#vue-todo-app',
-    data: {
-      taskInput: '',
-      todos: [
-        { id: 0, title: 'Learn JavaScript', active: '', display: true, completed: false },
-        { id: 1, title: 'Learn Vue', active: '', display: true, completed: false },
-        { id: 2, title: 'Build something awesome', active: '', display: true, completed: false }
-      ],
-      completedTasks: [],
-      selectedTask: null
-    },
-    mounted(){
-      window.addEventListener('keydown', (event)=>{
-        if(event.key == 'Enter') {
-          console.log('Add task!')
-          this.saveTask()
-          this.clearInput()
-        }
-      })
-      if(localStorage.getItem('todos'))
-        this.todos = JSON.parse(localStorage.getItem('todos'))
-    },
-    methods: {
-      clearInput(){
-        this.taskInput = ''
-      },
-      saveTask(){
-        if(this.taskInput) {
-          if(this.selectedTask != null) {
-            this.todos[this.selectedTask] = {id: this.selectedTask, title: this.taskInput, active: '', display: true, completed: this.todos[this.selectedTask].completed}
-          } else {
-            // new task
-            this.todos.push({id: this.todos.length, title: this.taskInput, active: '', display: true, completed: false})
-          }
-        }
-        this.selectedTask = null   
-        localStorage.setItem('todos', JSON.stringify(this.todos))
-      },
-      selectTask(id){
-        console.log('select task')
-        this.todos.forEach((item, index)=>{
-          this.todos[index].active = ''
-        })
-        this.taskInput = this.todos[id].title
-        this.selectedTask = id
-        this.todos[id].active = 'active'
-        document.getElementById('todo-input').focus()
-      },
-      deleteTask(id) {
-        this.todos.forEach((item, index)=>{
-          this.todos[index].active = ''
-        })
-        this.selectedTask = null
-        this.todos[id].display = false
-        localStorage.setItem('todos', JSON.stringify(this.todos))
-      },
-      deselectList(){
+  el: '#vue-todo-app',
+  data: {
+    taskInput: '',
+    todos: [
+      { id: 0, title: 'Learn JavaScript', active: '', display: true, completed: false },
+      { id: 1, title: 'Learn Vue', active: '', display: true, completed: false },
+      { id: 2, title: 'Build something awesome', active: '', display: true, completed: false }
+    ],
+    completedTasks: [],
+    selectedTask: null
+  },
+  mounted() {
+    window.addEventListener('keydown', (event) => {
+      if (event.key == 'Enter') {
+        console.log('Add task!')
         this.saveTask()
-        this.todos.forEach((item, index)=>{
-          this.todos[index].active = ''
-        })
-        this.selectedTask = null
         this.clearInput()
-      },
-      saveCompletedTasks(id){
-        console.log(this.todos[id])
-        if(this.todos[id].completed) {
-          this.todos[id].completed = false
-        } else {
-          this.todos[id].completed = true
-        }
-        localStorage.setItem('todos', JSON.stringify(this.todos))
       }
-      
+    })
+    if (localStorage.getItem('todos'))
+      this.todos = JSON.parse(localStorage.getItem('todos'))
+  },
+  methods: {
+    clearInput() {
+      this.taskInput = ''
+    },
+    saveTask() {
+      if (this.taskInput) {
+        if (this.selectedTask != null) {
+          this.todos[this.selectedTask] = { id: this.selectedTask, title: this.taskInput, active: '', display: true, completed: this.todos[this.selectedTask].completed }
+        } else {
+          // new task
+          this.todos.push({ id: this.todos.length, title: this.taskInput, active: '', display: true, completed: false })
+        }
+      }
+      this.selectedTask = null
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    },
+    selectTask(id) {
+      console.log('select task')
+      this.todos.forEach((item, index) => {
+        this.todos[index].active = ''
+      })
+      this.taskInput = this.todos[id].title
+      this.selectedTask = id
+      this.todos[id].active = 'active'
+      document.getElementById('todo-input').focus()
+    },
+    deleteTask(id) {
+      this.todos.forEach((item, index) => {
+        this.todos[index].active = ''
+      })
+      this.selectedTask = null
+      this.clearInput()
+      this.todos[id].display = false
+      localStorage.setItem('todos', JSON.stringify(this.todos))
+    },
+    deselectList() {
+      this.saveTask()
+      this.todos.forEach((item, index) => {
+        this.todos[index].active = ''
+      })
+      this.selectedTask = null
+      this.clearInput()
+    },
+    saveCompletedTasks(id) {
+      console.log(this.todos[id])
+      if (this.todos[id].completed) {
+        this.todos[id].completed = false
+      } else {
+        this.todos[id].completed = true
+      }
+      localStorage.setItem('todos', JSON.stringify(this.todos))
     }
-  })
+
+  }
+})
